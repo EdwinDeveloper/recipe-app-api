425ce83f12dfb825c97b7b1225499238677a175e
edwin <edwin.perez@rappi.com>
2022-05-27 17:30:47 -0500

    test

--- app/test.py
+++ app/test.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
 
-from calc import add
+from calc import add, substract
+
 
 class CalcTest(TestCase):
-    
+
     def test_add_numbers(self):
         """Test that two numbers are added toguether"""
         self.assertEqual(add(3,8),11)
+
+    def test_substract_numbers(self):
+        """Substract number"""
+        self.assertEqual(substract(8,4),4)
