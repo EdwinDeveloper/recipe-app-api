b876801844dfccc5629eeaf3aa42c438b7d291b7
Edwin Giovanni <edwingiovanni@MacBook-Pro-de-Edwin.local>
2022-05-28 13:28:20 -0500

    first migration

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -129,6 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
